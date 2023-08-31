0d2ed32fda247af6478c9cb7236279ba4f3beb74
charishma0342 <charishmadaggupati@gmail.com>
2023-08-30 23:46:45 -0500

    dashboard changes

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,55 +1,178 @@
-import React from "react";
+import React, { useEffect, useState, Suspense } from "react";
+import './Dashboard.css';
+import {
+  PieChart,
+  Pie,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  BarChart,
+  Bar,
+  Cell,
+  ResponsiveContainer,
+} from 'recharts';
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const Dashboard = () => {
-  const cards = [
-    {
-      title: "Interviews Conducted",
-      value: 2230,
-      icon: "fas fa-user-tie",
-    },
-    {
-      title: "Average Interview Score",
-      value: 74,
-      icon: "fas fa-chart-line",
-    },
-    {
-      title: "Skill Gap Rate",
-      value: 23,
-      icon: "fas fa-exclamation-triangle",
-    },
-    {
-      title: "Department Wise Participation",
-      value: 650,
-      icon: "fas fa-users",
-    },
-    {
-      title: "Department Wise Improvement Rate",
-      value: 5,
-      icon: "fas fa-arrow-up",
-    },
-    {
-      title: "Critical Improvement Areas",
-      value: "Communication, Technical Skills",
-      icon: "fas fa-exclamation-circle",
-    },
-  ];
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
+const data1 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.imageSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+ function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <div className="card-group">
-        {cards.map((card) => (
-          <div className="card" key={card.title}>
-            <div className="card-body">
-              <h5 className="card-title">{card.title}</h5>
-              <p className="card-text">{card.value}</p>
-              <i className={card.icon} />
-            </div>
-          </div>
-        ))}
+      <div className="card-container">
+        <Card
+          imageSrc="https://example.com/card1.jpg"
+          title="# of students enrolled"
+          description="600"
+        />
+        <Card
+          imageSrc="https://example.com/card2.jpg"
+          title="Interview Conducted"
+          description="This is the second card."
+        />
+        <Card
+          imageSrc="https://example.com/card3.jpg"
+          title="Improvement areas Identified"
+          description="This is the third card."
+        />
+      </div>
+      <div className="container">
+        <div className="placeholder">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+            <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+          </BarChart>
+        </div>
+        <div className="placeholder">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="basic"
+              dataKey="pv"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="basic" dataKey="uv" stroke="red" />
+          </LineChart>
+        </div>
+        <div className="placeholder">
+          <PieChart width={400} height={400}>
+          <Legend layout="vertical" verticalAlign="middle" align="right" />
+            <Pie
+            align="center"
+              data={data1}
+              cx={120}
+              cy={200}
+              innerRadius={60}
+              outerRadius={80}
+              fill="#8884d8"
+              paddingAngle={5}
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Dashboard;
