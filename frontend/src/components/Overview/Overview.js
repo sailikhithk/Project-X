25846d6a5c25de2cc0dbb167a5db714c0f9327ca
Sai Likhith Kanuparthi <slk522@nyu.edu>
2023-09-12 03:55:12 -0500

    updated dashboard

--- frontend/src/components/Overview/Overview.js
+++ frontend/src/components/Overview/Overview.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Overview = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [profileDetails, setProfileDetails] = useState({
+    institutionName: "MIT",
+    contactName: "John Doe",
+    contactEmail: "johndoe@example.com",
+    contactPhone: "123-456-7890",
+    numberOfStudents: "5000",
+    institutionPOC: "Jane Doe",
+    numberOfDepartments: "12",
+    country: "USA",
+    city: "Boston",
+    contactDesignation: "Manager",
+    registrationNumber: "12345",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProfileDetails({
+      ...profileDetails,
+      [name]: value,
+    });
+  };
+
+  const saveChanges = () => {
+    // Here you can send the updated profile details to the backend
+    console.log("Sending to backend:", profileDetails);
+    setIsEditing(false);
+  };
+
   return (
     <div className="content d-flex flex-column flex-column-fluid fs-6">
       <div className="container-xxl">
@@ -10,93 +39,43 @@ const Overview = () => {
             <div className="card-title m-0">
               <h4 className="fw-bold m-0">Profile Details</h4>
             </div>
-            <Link
+            <button
               className="btn btn-sm btn-primary align-self-center"
-              to={"/account/settings"}
+              onClick={() => setIsEditing(!isEditing)}
             >
-              Edit Profile
-            </Link>
+              {isEditing ? "Cancel" : "Edit Profile"}
+            </button>
           </div>
           <div className="card-body p-9">
-            <div className="row mb-2 mt-1">
-              <label className="col-lg-5 fw-semibold">Institution Name</label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
+            {Object.keys(profileDetails).map((key, index) => (
+              <div className="row mb-2 mt-1" key={index}>
+                <label className="col-lg-5 fw-semibold">
+                  {key.replace(/([A-Z])/g, " $1")}
+                </label>
+                <div className="col-lg-7">
+                  {isEditing ? (
+                    <input
+                      type="text"
+                      name={key}
+                      value={profileDetails[key]}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <span className="fw-normal fs-6">
+                      {profileDetails[key]}
+                    </span>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">Contact Name</label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">
-                Contact Email Address
-              </label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">
-                Contact Phone Number
-              </label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">Number of Students</label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">
-                Institution POC (Point Of Contact)
-              </label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">
-                Number of Departments
-              </label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">Country</label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">City</label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">
-                Contact's Designation
-              </label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
-            <div className="row mb-2 mt-3">
-              <label className="col-lg-5 fw-semibold">
-                Registration Number
-              </label>
-              <div className="col-lg-7">
-                <span className="fw-normal fs-6">ABCD</span>
-              </div>
-            </div>
+            ))}
           </div>
+          {isEditing && (
+            <div className="card-footer">
+              <button className="btn btn-sm btn-primary" onClick={saveChanges}>
+                Save Changes
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/components/Settings/Settings.js
+++ frontend/src/components/Settings/Settings.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const [isEditing, setIsEditing] = useState(false);
+  const [userSettings, setUserSettings] = useState({
+    username: "john_doe",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserSettings({
+      ...userSettings,
+      [name]: value,
+    });
+  };
+
+  const saveChanges = () => {
+    // Here you can send the updated username to the backend
+    console.log("Sending to backend:", userSettings);
+    setIsEditing(false);
+  };
+
+  const resetPassword = () => {
+    // Handle password reset here, e.g., open a modal for password reset
+    console.log("Resetting password...");
+  };
+
+  return (
+    <div className="content d-flex flex-column flex-column-fluid fs-6">
+      <div className="container-xxl">
+        <div className="card mb-xl-10">
+          <div className="d-flex justify-content-between align-items-center card-header bg-white cursor-pointer">
+            <div className="card-title m-0">
+              <h4 className="fw-bold m-0">Settings</h4>
+            </div>
+            <button
+              className="btn btn-sm btn-primary align-self-center"
+              onClick={() => setIsEditing(!isEditing)}
+            >
+              {isEditing ? "Cancel" : "Edit Settings"}
+            </button>
+          </div>
+          <div className="card-body p-9">
+            <div className="row mb-2 mt-1">
+              <label className="col-lg-5 fw-semibold">Username</label>
+              <div className="col-lg-7">
+                {isEditing ? (
+                  <input
+                    type="text"
+                    name="username"
+                    value={userSettings.username}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  <span className="fw-normal fs-6">
+                    {userSettings.username}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className="row mb-2 mt-3">
+              <label className="col-lg-5 fw-semibold">Reset Password</label>
+              <div className="col-lg-7">
+                <button
+                  className="btn btn-sm btn-warning"
+                  onClick={resetPassword}
+                >
+                  Reset Password
+                </button>
+              </div>
+            </div>
+          </div>
+          {isEditing && (
+            <div className="card-footer">
+              <button className="btn btn-sm btn-primary" onClick={saveChanges}>
+                Save Changes
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
