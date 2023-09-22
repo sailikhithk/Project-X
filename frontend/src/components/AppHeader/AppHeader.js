28552c032a03dab71e9b78790f6e5587fa0f5811
Sai Likhith Kanuparthi <slk522@nyu.edu>
2023-09-22 18:37:10 +0530

    AppHeader modified

--- frontend/src/components/AppHeader/AppHeader.js
+++ frontend/src/components/AppHeader/AppHeader.js
@@ -1,7 +1,36 @@
-import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const AppHeader = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (type) => {
+    const navigationUrl =
+      type === "PROFILE"
+        ? "/account/overview"
+        : type === "SETTINGS"
+        ? "/account/settings"
+        : "/";
+    navigate(navigationUrl, { replace: type === "LOGOUT" ? true : false });
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -15,6 +44,19 @@ const AppHeader = () => {
     >
       <Toolbar>
         <Typography variant="h6">Hello Christ University!</Typography>
+        <Box flexGrow={1} />
+        <IconButton edge="end" color="inherit" onClick={handleClick}>
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => handleClose("PROFILE")}>Profile</MenuItem>
+          <MenuItem onClick={() => handleClose("SETTINGS")}>Settings</MenuItem>
+          <MenuItem onClick={() => handleClose("LOGOUT")}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
